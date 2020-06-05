566de1254a005b2eabd7cf5a4753f214bdf93328
paul-goodall <paul.thomas.goodall@gmail.com>
2020-06-05 15:10:34 +1000

    Paul update

--- set-up-mac.sh
+++ set-up-mac.sh
@@ -2,6 +2,18 @@
 echo "Running script using bash version:"
 echo $BASH_VERSION
 
+# =================================
+# User settings:
+my_dir=$HOME/Mac_Setup
+DEFAULT_EMAIL="paul.thomas.goodall@gmail.com"
+
+GIT_NAME="paul-goodall"
+GIT_EMAIL=$DEFAULT_EMAIL
+SSH_EMAIL=$DEFAULT_EMAIL
+
+my_screenschots_dir=$HOME/Screenshots
+# =================================
+
 # This is my set-up, my way
 # If you're not me, *really* think before running this script--it's all on you
 
@@ -11,21 +23,19 @@ echo "Here we go ..."
 
 
 # Make my directories
-echo "Making my directories under HOME (~), i.e. under $HOME"
-mkdir ~/bin
-mkdir ~/blog
-mkdir ~/iso
-mkdir ~/lab
-mkdir ~/tmp
-mkdir ~/vm-share
-echo "Directory structure under HOME (~) is now:"
-ls -d */
+
+echo "Making my directories under ${my_dir}"
+mkdir -p $my_screenschots_dir
+mkdir -p $my_dir/bin
+mkdir -p $my_dir/tmp
+mkdir -p $my_dir/vm-share
+echo "Directory structure under ${my_dir} is now:"
+ls -d $my_dir/*
 
 
 # SSH keys
 echo "Generating SSH keys"
-echo "You will be prompted for email, file location (enter for default) and passphrase"
-read -p "Enter SSH key email: " SSH_EMAIL
+echo "You will be prompted for file location (enter for default) and passphrase"
 ssh-keygen -t rsa -b 4096 -C "$SSH_EMAIL"
 echo "Adding SSH private key to ssh-agent and storing passphrase in keychain"
 echo "You will be prompted for the passphrase again"
@@ -42,10 +52,14 @@ ssh-add -K ~/.ssh/id_rsa
 
 ## Install Homebrew itself
 echo "Installing Homebrew ..."
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 brew update
 brew upgrade
 echo "Done ..."
+
+# Fixing some issues with previous ZSH install
+sudo chown -R $(whoami) /usr/local/share/zsh /usr/local/share/zsh/site-functions
+chmod u+w /usr/local/share/zsh /usr/local/share/zsh/site-functions
 
 
 # Install packages and software using Homebrew
@@ -78,7 +92,8 @@ brew cask install hyper
 
 # Python (Homebrew version)
 brew install python
-pip install --upgrade pip
+#pip install --upgrade pip
+sudo -H pip3 install --upgrade pip
 
 # Dev tools
 brew install git
@@ -88,7 +103,6 @@ echo "Installing PowerShell Core. You will be prompted for root password."
 brew cask install powershell
 
 # Productivity
-brew cask install microsoft-teams
 brew cask install alfred
 brew cask install google-chrome
 brew cask install firefox
@@ -96,6 +110,47 @@ brew cask install dropbox
 brew cask install cisco-proximity
 brew cask install webex-teams
 brew cask install balenaetcher
+
+# Install Paul's things:
+brew cask install teamviewer
+brew cask install skype
+brew cask install mactex
+brew cask install vlc
+brew cask install zoom
+brew cask install gimp
+brew cask install whatsapp
+brew cask install calibre
+brew cask install steam
+brew cask install dropbox
+brew cask install evernote
+brew cask install vnc-viewer
+brew cask install kindle
+brew cask install lastpass
+brew cask install onedrive
+brew cask install parallels
+brew cask install expressvpn
+brew cask install duet
+brew cask install dosbox
+brew cask install omnifocus
+brew cask install kodi
+brew cask install vivaldi
+brew cask install cleanmymac
+brew cask install zettlr
+brew cask install flux
+brew cask install typora
+brew cask install openscad
+brew cask install sdformatter
+brew cask install texshop
+
+
+# Install Microsoft Stuff:
+brew cask install microsoft-teams
+brew cask install microsoft-excel
+brew cask install microsoft-word
+brew cask install microsoft-outlook
+brew cask install microsoft-powerpoint
+brew cask install onedrive
+
 
 # R
 
@@ -121,6 +176,8 @@ ln -sf \
 echo "To restore the default BLAS that comes with R use:"
 echo "  $ ln -sf /Library/Frameworks/R.framework/Versions/Current/Resources/lib/libRblas.0.dylib /Library/Frameworks/R.framework/Versions/Current/Resources/lib/libRblas.dylib"
 
+brew cask install rstudio
+
 # Not yet sure if need to do anything about linknig the LAPACK
 
 # Microsoft R Open
@@ -133,6 +190,8 @@ echo "  $ ln -sf /Library/Frameworks/R.framework/Versions/Current/Resources/lib/
 brew install node
 
 # Text editors and IDEs
+brew install emacs
+brew cask install aquamacs
 brew cask install visual-studio-code
 brew cask install sublime-text
 brew cask install rstudio
@@ -155,31 +214,67 @@ brew install hugo
 # Misc
 brew cask install spotify
 
-# Mac tools
+## Mac tools
 brew cask install scroll-reverser
-brew cask install sizeup
+# This didn't work for me - the certificate was broken.
+#brew cask install sizeup
+# Installing manually:
+wget https://www.irradiatedsoftware.com/downloads/SizeUp_1.7.4.zip -P $my_dir/tmp
+unzip -d /Applications/ $my_dir/tmp/SizeUp_1.7.4.zip
+rm $my_dir/tmp/SizeUp_1.7.4.zip
 
 # Homebrew installations complete
 brew cleanup
 echo "Homebrew software installations complete"
 
+# Backup any shell files before conda messes with them:
+if [ -e ~/.zshrc ]
+ then cp ~/.zshrc ~/zshrc.bak
+fi
+if [ -e ~/.bashrc ]
+ then cp ~/.bashrc ~/bashrc.bak
+fi
+if [ -e ~/.bash_profile ]
+ then cp ~/.bash_profile ~/bash_profile.bak
+fi
 
 # Conda
 echo "Installing Miniconda using their bash script (not Homebrew). You will be prompted multiple times."
-wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -P ~/tmp
-bash ~/tmp/Miniconda3-latest-MacOSX-x86_64.sh
-rm ~/tmp/Miniconda3-latest-MacOSX-x86_64.sh
+wget https://repo.anaconda.com/miniconda/Miniconda3-latest-MacOSX-x86_64.sh -P $my_dir/tmp
+bash $my_dir/tmp/Miniconda3-latest-MacOSX-x86_64.sh
+rm $my_dir/tmp/Miniconda3-latest-MacOSX-x86_64.sh
 
+my_shell="${SHELL}"
+echo "my shell: ${my_shell}"
+if [ $my_shell = "/bin/bash" ]
+then
 # Conda adds content to .bash_profile, but we want to manually call that when turning Conda on
 # So put all that stuff into another script, and we'll get .bash_profile later
-mv ~/.bash_profile ~/bin/conda-on-bash.sh
-echo "echo \"Conda ready to use\"" >> ~/bin/conda-on-bash.sh
+mv ~/.bash_profile  $my_dir/bin/conda-on-bash.sh
+mv ~/bash_profile.bak ~/.bash_profile
+echo "echo \"Conda ready to use\"" >> $my_dir/bin/conda-on-bash.sh
 
 # Since we're also using Zsh, we want a version of this script works in Zsh
-sed 's/bash/zsh/' ~/bin/conda-on-bash.sh > ~/bin/conda-on-zsh.sh
+sed 's/bash/zsh/'  $my_dir/bin/conda-on-bash.sh > $my_dir/bin/conda-on-zsh.sh
 
 # Turn on Conda to configure and to install some stuff
-source ~/bin/conda-on-bash.sh
+source  $my_dir/bin/conda-on-bash.sh
+fi
+
+if [ $my_shell = "/bin/zsh" ]
+then
+# Conda adds content to .bash_profile, but we want to manually call that when turning Conda on
+# So put all that stuff into another script, and we'll get .bash_profile later
+mv ~/.zshrc  $my_dir/bin/conda-on-zsh.sh
+mv ~/zshrc.bak ~/.zshrc
+echo "echo \"Conda ready to use\"" >> $my_dir/bin/conda-on-zsh.sh
+
+# Since we're also using Bash, we want a version of this script works in Zsh
+sed 's/zsh/bash/'  $my_dir/bin/conda-on-zsh.sh > $my_dir/bin/conda-on-bash.sh
+
+# Turn on Conda to configure and to install some stuff
+source  $my_dir/bin/conda-on-zsh.sh
+fi
 
 conda update conda
 conda --version
@@ -193,17 +288,17 @@ conda install --channel conda-forge --name base --yes jupyterlab
 conda install --name base --yes nb_conda_kernels
 
 # Sandbox Python environment
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/python-sandbox-env.yml -P ~/tmp
-conda env create --file ~/tmp/python-sandbox-env.yml
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/python-sandbox-env.yml -P  $my_dir/tmp
+conda env create --file  $my_dir/tmp/python-sandbox-env.yml
 conda activate base
-rm ~/tmp/python-sandbox-env.yml
+rm  $my_dir/tmp/python-sandbox-env.yml
 
 # Install IRkernel so can use R in Jupyter
 # This needs to be done while conda base environment is active, because it needs to see the Jupyter installation
 conda activate base
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/install-irkernel.R -P ~/tmp
-Rscript --verbose --vanilla ~/tmp/install-irkernel.R
-rm ~/tmp/install-irkernel.R
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/install-irkernel.R -P  $my_dir/tmp
+Rscript --verbose --vanilla  $my_dir/tmp/install-irkernel.R
+rm  $my_dir/tmp/install-irkernel.R
 
 # Clean up conda
 conda activate base
@@ -213,26 +308,26 @@ echo "List of conda environments now on your system"
 conda info --envs
 
 # Turn off conda
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/conda-off-bash.sh -P ~/bin
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/conda-off-zsh.sh -P ~/bin
-source ~/bin/conda-off-bash.sh
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/conda-off-bash.sh -P $my_dir/bin
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/conda-off-zsh.sh -P $my_dir/bin
+source $my_dir/bin/conda-off-bash.sh
+
 
 
 # Configure Git
 echo "Configuring Git settings and aliases ..."
-read -p "Enter global default Git email: " GIT_EMAIL
 
 # Configure Git settings
-git config --global user.name "Rob Jarvis"
-git config --global user.email "$GIT_EMAIL"
-git config --global core.editor "vim"
+git config --global user.name "${GIT_NAME}"
+git config --global user.email "${GIT_EMAIL}"
+git config --global core.editor "emacs"
 
 # Git aliases
 git config --global alias.unstage 'reset HEAD --' 
 git config --global alias.unmod 'checkout --' 
 git config --global alias.last 'log -1 HEAD' 
 git config --global alias.pub 'push -u origin HEAD' 
-git config --global alias.setemail 'config user.email jarvisrob@users.noreply.github.com' 
+git config --global alias.setemail 'config user.email ${GIT_NAME}@users.noreply.github.com' 
 git config --global alias.cm 'commit -m' 
 git config --global alias.co checkout 
 git config --global alias.cob 'checkout -b' 
@@ -247,13 +342,13 @@ echo "... Done"
 
 # Fonts
 brew tap homebrew/cask-fonts
-brew cask install font-meslo-nerd-font
+brew cask install font-meslolg-nerd-font
 
 
 # Oh My Zsh
 echo 'Installing Oh My Zsh'
-wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -P ~/tmp/ohmyzsh
-sh ~/tmp/ohmyzsh/install.sh --unattended
+wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -P $my_dir/tmp/ohmyzsh
+sh $my_dir/tmp/ohmyzsh/install.sh --unattended
 # --unattended: sets both CHSH and RUNZSH to 'no
 #   CHSH    - 'no' means the installer will not change the default shell (default: yes)
 #   RUNZSH  - 'no' means the installer will not run zsh after the install (default: yes)
@@ -261,7 +356,7 @@ sh ~/tmp/ohmyzsh/install.sh --unattended
 # Basically, you want to use this argument when installing from a script where you also install other things
 # We'll change the default shell to Zsh later and we don't want to launch a new shell straight away--
 # we want to keep doing other stuff
-rm -rf ~/tmp/ohmyzsh
+rm -rf $my_dir/tmp/ohmyzsh
 
 
 # macOS settings
@@ -286,7 +381,7 @@ defaults write com.apple.finder QuitMenuItem -bool true
 
 # Use list view in all Finder windows by default
 # Four-letter codes for the other view modes: `icnv`, `clmv`, `Flwv`
-defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder FXPreferredViewStyle -string "clmv"
 
 killall -HUP Finder
 
@@ -300,16 +395,16 @@ defaults write com.apple.dock autohide-delay -float 0
 defaults write com.apple.dock autohide -bool true
 
 # Only Show Open Applications In The Dock  
-defaults write com.apple.dock static-only -bool true
+defaults write com.apple.dock static-only -bool false
 
 # Minimise to Dock using "scale" effect
 defaults write com.apple.dock mineffect -string scale
 
-defaults write com.apple.dock orientation -string left
+defaults write com.apple.dock orientation -string bottom
 
 defaults write com.apple.dock magnification -bool false
 
-defaults write com.apple.dock show-process-indicators -bool false
+defaults write com.apple.dock show-process-indicators -bool true
 
 defaults write com.apple.dock tilesize -float 40
 
@@ -341,7 +436,7 @@ defaults write com.apple.screensaver askForPasswordDelay -int 0
 echo "Screenshot settings"
 
 # Save screenshots to the desktop
-defaults write com.apple.screencapture location -string "$HOME/Desktop"
+defaults write com.apple.screencapture location -string "${my_screenschots_dir}"
 
 # Save screenshots in PNG format (other options: BMP, GIF, JPG, PDF, TIFF)
 defaults write com.apple.screencapture type -string "png"
@@ -368,52 +463,52 @@ echo "Download dot files"
 
 # .aliases
 echo "Downloading .aliases"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.aliases -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.aliases -P ~
 cat ~/.aliases
 
 # .profile
 echo "Downloading .profile"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.profile -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.profile -P ~
 cat ~/.profile
 
 # .bash_profile
 echo "Downloading .bash_profile"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.bash_profile -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.bash_profile -P ~
 cat ~/.bash_profile
 
 # .bashrc
 echo "Downloading .bashrc"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.bashrc -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.bashrc -P ~
 cat ~/.bashrc
 
 # .zshenv
 echo "Downloading .zshenv"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.zshenv -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.zshenv -P ~
 cat ~/.zshenv
 
 # .zprofile
 echo "Downloading .zprofile"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.zprofile -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.zprofile -P ~
 cat ~/.zprofile
 
 # .zshrc
 echo "Downloading .zshrc"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.zshrc -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.zshrc -P ~
 cat ~/.zshrc
 
 # .vimrc (Vim)
 echo "Downloading .vimrc"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.vimrc -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.vimrc -P ~
 cat ~/.vimrc
 
 # .condarc (Conda)
 echo "Downloading .condarc"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.condarc -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.condarc -P ~
 cat ~/.condarc
 
 # .git-prompt-colors.sh (bash-git-prompt)
 echo "Downloading .git-prompt-colors.sh"
-wget https://raw.githubusercontent.com/jarvisrob/set-up-mac/master/.git-prompt-colors.sh -P ~
+wget https://raw.githubusercontent.com/${GIT_NAME}/set-up-mac/master/.git-prompt-colors.sh -P ~
 cat ~/.git-prompt-colors.sh
 
 
